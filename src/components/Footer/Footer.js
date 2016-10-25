fa2aca6a1218a87d3c74cdc0043648135a963a68
Drew <drew@pomerleaudesign.com>
2016-10-24 22:04:35 -0400

    fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,16 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Footer.scss';
 
+function scrollToTop() {
+  $("html, body").animate({ scrollTop: 0 });
+}
+
 function Footer() {
   return (
     <footer className="footer">
-      <button className="top-btn">Top</button>
-      <p>Made with &#10084; in Boston, &copy; 2016</p>
+      <span className="pull-left"><a href="https://github.com/Drewpom/my-website">Source Code</a></span>
+      <button className="top-btn" onClick={() => {scrollToTop()}}>Top</button>
+      <p>&copy; 2016</p>
     </footer>
   )
 }
